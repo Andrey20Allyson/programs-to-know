e4fae99cd264c80a6ff7089c3de9a6f91e47f6f6
Andrey Allyson <andreyuser47@gmail.com>
2022-12-07 18:12:03 -0300

    DataBase implementation

--- src/server/api/storage/tecs.ts
+++ src/server/api/storage/tecs.ts
@@ -1,30 +1,55 @@
 import { Router } from 'express';
-import { DownloadItemOptions } from '../../../../public/src/loader/DownloadItem.d'
+import { DataBase } from '../../database';
+import { OkPacket } from 'mysql';
+
+export interface ITecnologiesDBColumns {
+    [k: string]: string;
+}
+
+export const tecnologiesDBColumns: ITecnologiesDBColumns = {
+    title: 'title',
+    downloadLink: 'downloadLink',
+    imageUrl: 'imageUrl',
+    description: 'desc'
+};
 
 export function StorageRoute() {
     const router = Router();
+    const dataBase = DataBase.createDataBase({
+        user: 'root',
+        database: 'programs-to-know-db'
+    });
 
     router.get('/tecs', async (req, res, next) => {
         const { type } = req.query;
     
-        if (type === 'json') {
-            // const value = await readFile('./data/tecnologies.json', { encoding: 'utf-8' });
-    
-            // res.json(JSON.parse(value));
-        }
+        if (type === 'json') {}
     });
     
     router.post('/tecs', async (req, res, next) => {
         const { data } = req.query;
     
         if (typeof data !== 'string') return;
-    
+
         try {
-            const object: DownloadItemOptions = JSON.parse(data);
-    
+            // const object: any = JSON.parse(data);
+
+            // const columns: string[] = [];
+            // const values: string[] = [];
+
+            // for (const key in tecnologiesDBColumns) {
+            //     const prop = tecnologiesDBColumns[key];
+
+            //     if (object[prop]) {
+            //         columns.push(key);
+            //         values.push(typeof object[prop] === 'string'? `"${object[prop]}"`: `${object[prop]}`);
+            //     }
+            // }
+
+            // const res = await dataBase.query<OkPacket>(`insert into tecnologies (${columns.toString()}) values (${values.toString()});`);
             
         } catch(err) {
-    
+            console.log(err);
         }
     });
 

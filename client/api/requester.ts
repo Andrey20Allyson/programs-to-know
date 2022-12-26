cce47d6570b0b4b41334eb0288d34550eaa8e763
Andrey Allyson <andreyuser47@gmail.com>
2022-12-26 15:28:09 -0300

    register script + requester fixes

--- client/api/requester.ts
+++ client/api/requester.ts
@@ -1,7 +1,10 @@
 import { TecsRequester } from './lib/TecsRequester.js';
+import { ArchitectureEnum, TecnologyDTO } from './lib/TecsRequester.d';
 
 export function createTecsRequester(): TecsRequester {
-    const tecsRequester = new TecsRequester('./api/storage/tecs');
+    const tecsRequester = new TecsRequester(document.location.origin + '/api/storage/tecs');
 
     return tecsRequester;
 }
+
+export { ArchitectureEnum, TecnologyDTO };
--- client/register.ts
+++ client/register.ts
@@ -1,9 +1,51 @@
+import { createTecsRequester, ArchitectureEnum } from './api/requester.js';
+
 function getInput(id: string) {
     const input = document.getElementById(id);
     if (input instanceof HTMLInputElement) return input;
 
-    throw new Error();
+    throw new Error('Element isn\'t a HTMLInputElement');
 }
 
-const titleInputElement = getInput('title-input');
-const downloadUrlInputElement = getInput('downloadUrl-input');
+function getSelect(id: string) {
+    const select = document.getElementById(id);
+    if (select instanceof HTMLSelectElement) return select;
+
+    throw new Error('Element isn\'t a HTMLSelectElement');
+}
+
+function getArchitectureEnum(value: string): ArchitectureEnum | null {
+    switch (value) {
+        case '64':
+            return value;
+        case '86':
+            return value;
+        default:
+            return null;
+    }
+}
+
+const inputs = {
+    title: getInput('title-input'),
+    downloadUrl: getInput('downloadUrl-input'),
+    imageUrl: getInput('imageUrl-input'),
+    dependences: getInput('dependences-input'),
+    description: getInput('description-input'),
+    architecture: getSelect('architecture-select'),
+    confirmBtn: getInput('register-button')
+}
+
+const requester = createTecsRequester();
+
+inputs.confirmBtn.addEventListener('click', () => {
+    requester.post([
+        {
+            title: inputs.title.value || null,
+            downloadUrl: inputs.downloadUrl.value || null,
+            imageUrl: inputs.imageUrl.value || null,
+            dependences: [],
+            architecture: getArchitectureEnum(inputs.architecture.value) || null,
+            description: inputs.description.value || null
+        }
+    ]);
+});

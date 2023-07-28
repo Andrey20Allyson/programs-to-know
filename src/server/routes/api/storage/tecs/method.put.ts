5a773d2fb1358e6eceaf29d1215f7ab09a383eaf
Andrey Allyson <andreyuser47@gmail.com>
2023-07-28 16:52:23 -0300

    update: tec storage PUT method fixes

--- src/server/routes/api/storage/tecs/method.put.ts
+++ src/server/routes/api/storage/tecs/method.put.ts
@@ -1,24 +1,43 @@
 import { RequestHandler } from "express";
 import { ParamsDictionary } from 'express-serve-static-core';
 import { BaseTecHandlerConfig } from ".";
-import { PutTecResponseBody, tecDTOSchema } from "./schemas";
+import { PutTecResponseBody, TecDatabaseDTO, tecDTOSchema } from "./schemas";
 
 export function createPutTecHandler(config: BaseTecHandlerConfig): RequestHandler<ParamsDictionary, PutTecResponseBody> {
   const {
-    database,
     collectionName,
+    database,
   } = config;
 
   return async (request, response) => {
     const bodyParseResult = tecDTOSchema.safeParse(request.body);
     if (!bodyParseResult.success) return response.status(400).send({ ok: false, error: 'Bad Request: invalid body' });
 
-    const tec = bodyParseResult.data;
+    const {
+      architecture,
+      dependences,
+      description,
+      downloadUrl,
+      searchName,
+      imageUrl,
+      title,
+      id,
+    } = bodyParseResult.data;
+
+    const tecDatabaseDTO: TecDatabaseDTO = {
+      architecture,
+      dependences,
+      description,
+      downloadUrl,
+      searchName,
+      imageUrl,
+      title,
+    };
 
     try {
-      await database.collection(collectionName).doc(tec.id).update(tec);
+      await database.collection(collectionName).doc(id).update(tecDatabaseDTO);
 
-      return response.status(200).send({ ok: true, data: { id: tec.id } });
+      return response.status(200).send({ ok: true, data: { id } });
     } catch (err) {
       console.error(err);
       const message = err instanceof Error ? err.message : JSON.stringify(err);
